613fbf08961ef91b9bf975a44f0ef15c01aace72
0nik0nik0 <119619487+0nik0nik0@users.noreply.github.com>
2024-01-25 21:00:02 +0300

    initial commit

---
+++ HW1/script.js
@@ -0,0 +1,9 @@
+// Task 1
+let tempCelsius = 23;
+let convertToFahrenheit = (9 / 5) * tempCelsius + 32;
+console.log(`Temperature ${tempCelsius} C = ${convertToFahrenheit} F`);
+
+// Task 2
+const name = "Nikita";
+const admin = name;
+console.log(`The value of the variable admin = ${admin}`);
